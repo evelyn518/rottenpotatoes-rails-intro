5c8ffc749d90690d7b5a9bb824febac9ada3b9bf
Ruilian Gao <evelyn_518@tamu.edu>
2021-02-26 04:52:23 +0000

    finsihed 3 parts testing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        a = Array.new
+        self.select("rating").uniq.each {|x| a.push(x.rating)}
+        a.sort.uniq
+    end
+    def self.with_ratings(ratings_list)
+        if ratings_list == nil
+            self.all_ratings
+        end
+        self.where("rating": ratings_list)
+    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+    #  movies with those ratings
+    # if ratings_list is nil, retrieve ALL movies
+    end
+
 end
